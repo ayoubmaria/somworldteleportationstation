44bb1710aef51859832649c915683b4c1961229d
ayoubmaria <120912058+ayoubmaria@users.noreply.github.com>
2022-12-18 15:01:42 +0400

    initial version.

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1618763.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 115556195, 115556203, 115556205, 115556219, 115556222, 115556229 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/115556188/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/115556189/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/115556187/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/115556192/1/basis.wasm.js', 'wasmUrl' : 'files/assets/115556193/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/115556191/1/basis.js', 'preload' : false},
+];
